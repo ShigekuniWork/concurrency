a7c5e315f002ae2a3154eeeb304e8c4e1c15262e
ShigekuniWork <shigekuni.work@gmail.com>
2026-02-09 07:46:44 +0900

    Refactor main function to utilize AtomicU32 for simpler increment logic
    - Replaced AtomicUsize and AtomicU64 with AtomicU32 for improved performance in counting.
    - Simplified the threading model by removing background threads and focusing on atomic increment operations.
    - Introduced a new increment function to handle atomic updates in a loop, enhancing clarity and maintainability.

--- rust/atomic/src/main.rs
+++ rust/atomic/src/main.rs
@@ -1,51 +1,23 @@
-use std::sync::atomic::AtomicU64;
-use std::time::{Duration, Instant};
-use std::{sync::atomic::AtomicUsize, thread};
-
+use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::Relaxed;
 
 fn main() {
-    let num_done = &AtomicUsize::new(0);
-    let total_time = &AtomicU64::new(0);
-    let max_time = &AtomicU64::new(0);
+    let num = AtomicU32::new(0);
+    increment(&num);
+    println!("num: {}", num.load(Relaxed));
+    increment(&num);
+    println!("num: {}", num.load(Relaxed));
+    increment(&num);
+    println!("num: {}", num.load(Relaxed));
+}
 
-    thread::scope(|s| {
-        // 4つのバックグラウンドスレッドがそれぞれ25アイテム、計100アイテムを処理する
-        for t in 0..4 {
-            s.spawn(move || {
-                for i in 0..25 {
-                    let start = Instant::now();
-                    for _ in 0..10 {
-                        let _ = t * 25 + i;
-                    }
-                    let time_taken = start.elapsed().as_micros() as u64;
-                    num_done.fetch_add(1, Relaxed);
-                    total_time.fetch_add(time_taken, Relaxed);
-                    max_time.fetch_max(time_taken, Relaxed);
-                }
-            });
+fn increment(num: &AtomicU32) {
+    let mut current = num.load(Relaxed);
+    loop {
+        let new = current + 1;
+        match num.compare_exchange(current, new, Relaxed, Relaxed) {
+            Ok(_) => break,
+            Err(x) => current = x,
         }
-
-        // メインスレッドは更新された状態を毎秒表示する
-        loop {
-            let total_time = Duration::from_micros(total_time.load(Relaxed));
-            let max_time = Duration::from_micros(max_time.load(Relaxed));
-            let n = num_done.load(Relaxed);
-            if n == 100 {
-                break;
-            }
-            if n == 0 {
-                println!("Working.. nothing done yet.");
-            } else {
-                println!(
-                    "Working.. {n}/100 done, {:?} average, {:?} peak",
-                    total_time / n as u32,
-                    max_time,
-                );
-            }
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    println!("Done!");
+    }
 }
