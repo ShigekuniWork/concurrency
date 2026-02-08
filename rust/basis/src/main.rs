e4c324bb8ae808d5d195af3c5e2f7812ded4172e
ShigekuniWork <shigekuni.work@gmail.com>
2026-02-08 21:20:31 +0900

    Add main Rust program with multithreading example
    - Implemented a main function that spawns multiple threads.
    - Added a thread-safe reference counting example using Rc.
    - Included a simple demonstration of thread creation and joining.
    - Printed messages from both the main thread and spawned threads.

---
+++ rust/basis/src/main.rs
@@ -0,0 +1,33 @@
+use std::{rc::Rc, thread};
+
+fn main() {
+    let t1 = thread::spawn(f);
+    let t2 = thread::spawn(f);
+
+    println!("Hello from the main thread!");
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    thread::spawn(move || {
+        for n in numbers {
+            println!("Number: {}", n);
+        }
+    })
+    .join()
+    .unwrap();
+
+    let a = Rc::new([1, 2, 3, 4, 5]);
+    let b = a.clone();
+
+    assert_eq!(a.as_ptr(), b.as_ptr());
+}
+
+fn f() {
+    println!("Hello from another thread!");
+
+    let id = thread::current().id();
+    println!("Thread id this thread is {:?}", id);
+}
