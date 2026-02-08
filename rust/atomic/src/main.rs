a475f3163ab83da5fd5d07a2b837a75e64933e0f
ShigekuniWork <shigekuni.work@gmail.com>
2026-02-09 00:26:41 +0900

    Enhance thread synchronization and progress reporting in main function
    - Introduced a main thread reference to allow for unblocking the main thread from the spawned thread.
    - Modified the spawned thread to perform additional computations within a loop.
    - Updated the progress reporting to display a more informative message and adjusted the sleep mechanism to use a timeout instead of a fixed duration.

--- rust/atomic/src/main.rs
+++ rust/atomic/src/main.rs
@@ -6,10 +6,16 @@ use std::sync::atomic::Ordering::Relaxed;
 fn main() {
     let num_done = AtomicUsize::new(0);
 
+    let main_thread = thread::current();
+
     thread::scope(|s| {
         s.spawn(|| {
             for _ in 0..100 {
+                for _ in 0..1000 {
+                    let _ = 1 + 1;
+                }
                 num_done.fetch_add(1, Relaxed);
+                main_thread.unpark();
             }
         });
 
@@ -18,8 +24,8 @@ fn main() {
             if n == 100 {
                 break;
             }
-            println!("Not done, n = {}", n);
-            thread::sleep(Duration::from_secs(1));
+            println!("Working... {n}/100 done");
+            thread::park_timeout(Duration::from_secs(1));
         }
     });
 
