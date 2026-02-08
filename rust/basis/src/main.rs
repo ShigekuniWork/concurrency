4ef33b1ec690db6060ed51ccc8177615a489c43e
ShigekuniWork <shigekuni.work@gmail.com>
2026-02-08 21:54:19 +0900

    Add Cell and RefCell examples in main function
    - Introduced Cell for interior mutability with a demonstration of value modification.
    - Added RefCell to showcase mutable borrowing of a vector.
    - Enhanced the main function with additional examples of Rust's memory management features.

--- rust/basis/src/main.rs
+++ rust/basis/src/main.rs
@@ -1,4 +1,4 @@
-use std::{rc::Rc, thread};
+use std::{cell::{Cell, RefCell}, rc::Rc, thread};
 
 fn main() {
     let t1 = thread::spawn(f);
@@ -23,6 +23,23 @@ fn main() {
     let b = a.clone();
 
     assert_eq!(a.as_ptr(), b.as_ptr());
+
+
+    // cell
+    let x = Cell::new(5);
+    let y = &x;
+    let z = &x;
+
+    y.set(6);
+    println!("x: {}", x.get());
+    println!("z: {}", z.get());
+
+    // RefCell
+    let v = RefCell::new(vec![1, 2, 3, 4, 5]);
+    let v_ref = &v;
+
+    v_ref.borrow_mut().push(6);
+    println!("v: {:?}", v_ref.borrow());
 }
 
 fn f() {
