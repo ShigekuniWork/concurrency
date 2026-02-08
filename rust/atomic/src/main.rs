47e66995940777858650721c72058e94545e9340
ShigekuniWork <shigekuni.work@gmail.com>
2026-02-09 07:35:50 +0900

    Refactor main function to enhance performance tracking and thread management
    - Introduced AtomicU64 for tracking total and maximum execution time across threads.
    - Updated thread spawning logic to process items in batches, improving efficiency.
    - Enhanced progress reporting to include average and peak execution times.
    - Adjusted the main thread's sleep mechanism for better responsiveness during execution.

--- rust/atomic/src/main.rs
+++ rust/atomic/src/main.rs
@@ -1,33 +1,51 @@
-use std::time::Duration;
+use std::sync::atomic::AtomicU64;
+use std::time::{Duration, Instant};
 use std::{sync::atomic::AtomicUsize, thread};
 
 use std::sync::atomic::Ordering::Relaxed;
 
 fn main() {
-    let num_done = AtomicUsize::new(0);
-
-    let main_thread = thread::current();
+    let num_done = &AtomicUsize::new(0);
+    let total_time = &AtomicU64::new(0);
+    let max_time = &AtomicU64::new(0);
 
     thread::scope(|s| {
-        s.spawn(|| {
-            for _ in 0..100 {
-                for _ in 0..1000 {
-                    let _ = 1 + 1;
+        // 4つのバックグラウンドスレッドがそれぞれ25アイテム、計100アイテムを処理する
+        for t in 0..4 {
+            s.spawn(move || {
+                for i in 0..25 {
+                    let start = Instant::now();
+                    for _ in 0..10 {
+                        let _ = t * 25 + i;
+                    }
+                    let time_taken = start.elapsed().as_micros() as u64;
+                    num_done.fetch_add(1, Relaxed);
+                    total_time.fetch_add(time_taken, Relaxed);
+                    max_time.fetch_max(time_taken, Relaxed);
                 }
-                num_done.fetch_add(1, Relaxed);
-                main_thread.unpark();
-            }
-        });
+            });
+        }
 
+        // メインスレッドは更新された状態を毎秒表示する
         loop {
+            let total_time = Duration::from_micros(total_time.load(Relaxed));
+            let max_time = Duration::from_micros(max_time.load(Relaxed));
             let n = num_done.load(Relaxed);
             if n == 100 {
                 break;
             }
-            println!("Working... {n}/100 done");
-            thread::park_timeout(Duration::from_secs(1));
+            if n == 0 {
+                println!("Working.. nothing done yet.");
+            } else {
+                println!(
+                    "Working.. {n}/100 done, {:?} average, {:?} peak",
+                    total_time / n as u32,
+                    max_time,
+                );
+            }
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
-    println!("Done")
+    println!("Done!");
 }
