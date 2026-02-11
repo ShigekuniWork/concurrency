861baa8222e03388e42de2a6822cfc09a5887416
ShigekuniWork <shigekuni.work@gmail.com>
2026-02-11 12:54:29 +0900

    Add self-async package for concurrent task management
    - Introduced a new package `self-async` that implements a custom task scheduler using `async-task` and `flume` for managing high and low priority tasks.
    - Updated `Cargo.toml` to include dependencies for `async-task`, `flume`, and `futures-lite`.
    - Enhanced the main function to demonstrate the scheduling of tasks with different priorities and included a `CounterFuture` for asynchronous counting.
    - Updated workspace configuration to include the new `self-async` package.

---
+++ rust/self-async/src/main.rs
@@ -0,0 +1,203 @@
+use std::pin::Pin;
+use std::sync::LazyLock;
+use std::task::{Context, Poll};
+use std::time::Duration;
+use std::{future::Future, panic::catch_unwind, thread};
+
+use async_task::{Runnable, Task};
+use flume::{Receiver, Sender};
+use futures_lite::future;
+
+fn spawn_task<F, T>(future: F, order: FutureType) -> Task<T>
+where
+    F: Future<Output = T> + Send + 'static,
+    T: Send + 'static,
+{
+    static HIGH_CHANNEL: LazyLock<(Sender<Runnable>, Receiver<Runnable>)> =
+        LazyLock::new(flume::unbounded::<Runnable>);
+    static LOW_CHANNEL: LazyLock<(Sender<Runnable>, Receiver<Runnable>)> =
+        LazyLock::new(flume::unbounded::<Runnable>);
+
+    static HIGH_QUEUE: LazyLock<flume::Sender<Runnable>> = LazyLock::new(|| {
+        let high_num = std::env::var("HIGH_NUM").unwrap().parse::<usize>().unwrap();
+        for _ in 0..high_num {
+            let high_receiver = HIGH_CHANNEL.1.clone();
+            let low_receiver = LOW_CHANNEL.1.clone();
+            thread::spawn(move || {
+                loop {
+                    match high_receiver.try_recv() {
+                        Ok(runnable) => {
+                            let _ = catch_unwind(|| runnable.run());
+                        }
+                        Err(_) => match low_receiver.try_recv() {
+                            Ok(runnable) => {
+                                let _ = catch_unwind(|| runnable.run());
+                            }
+                            Err(_) => {
+                                thread::sleep(Duration::from_millis(100));
+                            }
+                        },
+                    };
+                }
+            });
+        }
+        HIGH_CHANNEL.0.clone()
+    });
+    static LOW_QUEUE: LazyLock<flume::Sender<Runnable>> = LazyLock::new(|| {
+        let low_num = std::env::var("LOW_NUM").unwrap().parse::<usize>().unwrap();
+        for _ in 0..low_num {
+            let high_receiver = HIGH_CHANNEL.1.clone();
+            let low_receiver = LOW_CHANNEL.1.clone();
+            thread::spawn(move || {
+                loop {
+                    match low_receiver.try_recv() {
+                        Ok(runnable) => {
+                            let _ = catch_unwind(|| runnable.run());
+                        }
+                        Err(_) => match high_receiver.try_recv() {
+                            Ok(runnable) => {
+                                let _ = catch_unwind(|| runnable.run());
+                            }
+                            Err(_) => {
+                                thread::sleep(Duration::from_millis(100));
+                            }
+                        },
+                    };
+                }
+            });
+        }
+        LOW_CHANNEL.0.clone()
+    });
+
+    let schedule_high = |runnable| HIGH_QUEUE.send(runnable).unwrap();
+    let schedule_low = |runnable| LOW_QUEUE.send(runnable).unwrap();
+
+    let schedule = match order {
+        FutureType::High => schedule_high,
+        FutureType::Low => schedule_low,
+    };
+    let (runnable, task) = async_task::spawn(future, schedule);
+    runnable.schedule();
+    task
+}
+
+macro_rules! spawn_task {
+    ($future:expr) => {
+        spawn_task!($future, FutureType::Low)
+    };
+    ($future:expr, $order:expr) => {
+        spawn_task($future, $order)
+    };
+}
+
+#[derive(Debug, Clone, Copy)]
+
+enum FutureType {
+    High,
+    Low,
+}
+
+struct CounterFuture {
+    count: u32,
+}
+
+impl Future for CounterFuture {
+    type Output = u32;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.count += 1;
+        println!("polling with result: {}", self.count);
+        std::thread::sleep(Duration::from_secs(1));
+        if self.count < 3 {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        } else {
+            Poll::Ready(self.count)
+        }
+    }
+}
+
+async fn async_fn() {
+    std::thread::sleep(Duration::from_secs(1));
+    println!("async fn");
+}
+
+macro_rules! try_join {
+    ($($future:expr),*) => {
+        {
+            let mut results = Vec::new();
+            $(
+                let result = catch_unwind(|| future::block_on($future));
+                match result {
+                    Ok(val) => results.push(val),
+                    Err(_) => {
+                        panic!("A future panicked during execution.");
+                    }
+                }
+            )*
+            results
+        }
+    };
+}
+
+#[derive(Debug, Clone, Copy)]
+struct BackgroundProcess;
+
+impl Future for BackgroundProcess {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("background process firing");
+        std::thread::sleep(Duration::from_secs(1));
+        cx.waker().wake_by_ref();
+        Poll::Pending
+    }
+}
+struct Runtime {
+    high_num: usize,
+    low_num: usize,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        let num_cores = std::thread::available_parallelism().unwrap().get();
+        Self {
+            high_num: num_cores - 2,
+            low_num: 1,
+        }
+    }
+
+    pub fn run(&self) {
+        unsafe {
+            std::env::set_var("HIGH_NUM", self.high_num.to_string());
+            std::env::set_var("LOW_NUM", self.low_num.to_string());
+        }
+        let high = spawn_task!(async {}, FutureType::High);
+        let low = spawn_task!(async {}, FutureType::Low);
+        try_join!(high, low);
+    }
+}
+
+fn main() {
+    Runtime::new().run();
+    spawn_task!(BackgroundProcess {}).detach();
+
+    let one = CounterFuture { count: 0 };
+    let two = CounterFuture { count: 0 };
+
+    let t_one = spawn_task!(one, FutureType::High);
+    let t_two = spawn_task!(two, FutureType::Low);
+    let t_three = spawn_task!(async_fn());
+    let t_four = spawn_task!(
+        async {
+            async_fn().await;
+            async_fn().await;
+        },
+        FutureType::High
+    );
+
+    let outcome: Vec<u32> = try_join!(t_one, t_two);
+    let outcome_two: Vec<()> = try_join!(t_four, t_three);
+    println!("outcome: {:?}", outcome);
+    println!("outcome_two: {:?}", outcome_two);
+}
